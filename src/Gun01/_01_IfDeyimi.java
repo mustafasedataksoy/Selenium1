27312c4a40c187755e25ea41402bf16d6fed8225
TechnoStudy <ismettechno@gmail.com>
2022-07-05 17:49:00 +0100

    for dongusu örnek 1 yapıldı

--- src/Gun01/_01_IfDeyimi.java
+++ src/Gun01/_01_IfDeyimi.java
@@ -4,5 +4,9 @@ public class _01_IfDeyimi {
     public static void main(String[] args) {
         System.out.println("Merhaba dünya");
 
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
     }
 }
